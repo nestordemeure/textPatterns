acec34e879ed17733d7b92d35b921d5e7426e411
Nestor Demeure <nestordemeure@gmail.com>
2018-06-08 23:11:35 +0200

    checking complexity of intermediate functions

--- src/main.fsx
+++ src/main.fsx
@@ -1,4 +1,5 @@
 
+#load "pattern.fsx"
 #load "importation.fsx"
 #load "clustering/hierarchical.fsx"
 #load "output.fsx"
@@ -8,7 +9,7 @@
 
 /// takes a path, displays the tree builded from the associated logs
 let main path =
-   let logs = Importation.importLogs path
+   let logs = Importation.importLogs path |> List.take 30
    let tree = Hierarchical.buildTree logs
    Output.displayTree tree
 
@@ -17,5 +18,6 @@ let main path =
 
 printfn "Starting..."
 
-main "tests/kittens.txt"
+//main "tests/kittens.txt"
 //main "tests/log.txt"
+main "tests/kern.log" // taken from /var/log/kern.log
--- src/pattern.fsx
+++ src/pattern.fsx
@@ -14,13 +14,14 @@ type Pattern = Token list
 let universalPattern = [Unknown]
 
 /// returns true if the list of tokens matches the pattern
+/// TODO memoize for better efficiency
 let rec matchPattern pattern tokens =
    match pattern, tokens with
    | [], [] -> true
    | Unknown :: newPattern, _ :: newTokens ->
       (matchPattern newPattern newTokens) // match 1 token (redondant but usefull to express match with empty list)
       || (matchPattern pattern newTokens) // match 1+ tokens
-      || (matchPattern newPattern tokens) // match 0 token
+      //|| (matchPattern newPattern tokens) // match 0 token : more prone to introducing non intuitive things than to solve problems
    | p :: pattern, t :: tokens when p = t -> 
       matchPattern pattern tokens
    | _ -> false
@@ -29,21 +30,19 @@ let rec matchPattern pattern tokens =
 // SCORING
 
 /// represents the evalutation of a pattern
-/// specificity is (length-unknownCount)/length
-/// Note : the type is layed out so that comparaison will be by specificity and then length
+/// Note : the type is layed out by order of priority in comparaisons
 type Score = 
-   {specificity : float; length : int; unknownCount : int}
+   {known : int; negativ_length : int}
 
 /// returns the score of a pattern
 let score pattern =
    let mutable length = 0
-   let mutable unknownCount = 0
+   let mutable known = 0
    let count t =
       length <- length + 1
-      if t = Unknown then unknownCount <- unknownCount + 1
+      if t <> Unknown then known <- known + 1
    List.iter count pattern
-   let specificity = float (length - unknownCount) / float length
-   {specificity=specificity; length=length; unknownCount=unknownCount}
+   {known=known; negativ_length= -length}
 
 //-----------------------------------------------------------------------------
 // PATTERN BUILDING
@@ -65,18 +64,25 @@ let addToken token pattern =
    | _ -> [token :: pattern; Unknown :: pattern]
 
 /// returns the most specific pattern that matches both patterns
+/// NOTE uses memoization to reduce complexity to o(n²)
+/// TODO dynamic programming could greatly increase performances
 /// TODO we could gain some speed by propagating the scores in order to avoid recomputing them at each iterations
-/// TODO could be greatly speed up with memoization/dynamic programming
-let rec commonPattern pattern1 pattern2 =
-   match pattern1, pattern2 with
-   | [], [] -> []
-   | [], _ | _, [] -> [Unknown]
-   | t1::q1, t2::q2 when t1 = t2 ->
-      t1 :: commonPattern q1 q2
-   | _::q1, _::q2 ->
-      let drop1 = commonPattern q1 pattern2 |> addUnknown
-      let drop2 = commonPattern q2 pattern1 |> addUnknown
-      if score drop1 > score drop2 then drop1 else drop2
+let commonPattern pattern1 pattern2 =
+   let memory = System.Collections.Generic.Dictionary<int*int,Pattern>()
+   let rec father (depth1,depth2) pattern1 pattern2 =
+      if memory.ContainsKey (depth1,depth2) then memory.[(depth1,depth2)] else
+         match pattern1, pattern2 with
+         | [], [] -> []
+         | [], _ | _, [] -> [Unknown]
+         | t1::q1, t2::q2 when t1 = t2 ->
+            t1 :: father (depth1+1,depth2+1) q1 q2 // TODO memoize
+         | _::q1, _::q2 ->
+            let drop1 = father (depth1+1,depth2) q1 pattern2 |> addUnknown
+            let drop2 = father (depth1,depth2+1) pattern1 q2 |> addUnknown
+            let result = if score drop1 > score drop2 then drop1 else drop2
+            memory.[(depth1,depth2)] <- result
+            result
+   father (0,0) pattern1 pattern2
 
 /// takes a pattern and outputs a sequence of all the patterns that matches the current pattern
 /// WARNING : complexity 2^length(pattern)
